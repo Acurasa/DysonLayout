dd986f66bb2c1cc552a39ee88a197487cf0f2992
Acurasa <unxetra@gmail.com>
2024-02-04 05:15:48 +0100

    Add Vue project for GitHub Pages 1.3

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,6 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
-module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? '/DysonLayout/' : '/',
   transpileDependencies: true
-};
+});
